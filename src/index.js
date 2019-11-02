2fe3445d5f3b44d92ca0a53ca5030fe6c4cbd41c
William Philippe <wplb@ic.ufal.br>
2019-11-02 20:29:53 -0300

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 
 // import { Container } from './styles';
+import Routes from './routes';
 
 export default function App() {
-  return <Text>React Native</Text>;
+  return <Routes />;
 }
---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Image} from 'react-native';
+
+import logo from '~/assets/logo.png';
+
+import Background from '~/components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignIn() {
+  return (
+    <Background>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Seu melhor email"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
+    </Background>
+  );
+}
